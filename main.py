570ef65f018e1121d783642413d30d5110df6c99
Michael Hamm <mike.hamm@commentsold.com>
2023-05-01 22:58:51 -0400

    Updating information for recipes from FoodNetwork

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from recipe import Recipe
 from bs4 import BeautifulSoup
 
 foodNetworkURL = "https://www.foodnetwork.com/recipes/recipes-a-z/p/1"
@@ -6,40 +7,69 @@ page1 = requests.get(foodNetworkURL)
 
 soup = BeautifulSoup(page1.content, "html.parser")
 
-listOfPages = soup.find_all("li", {"class": 'm-PromoList__a-ListItem'})
+source = str
+website = str
+total_time = str
+fnList = []
 newList = []
+
+listOfPages = soup.find_all("li", {"class": 'm-PromoList__a-ListItem'})
 for entry in listOfPages:
     entry1 = (entry.find('a')["href"]).replace("//", "")
     newList.append(entry1)
 
-# print(newList)
 
-fnList = []
-
-
-class Recipe:
-    id: int
-    title: str
-    source: str
-    website: str
-
-
-for index in range(0, len(newList), 10):
+for index in range(0, 1):
     recipe = Recipe()
-    print("https://" + newList[index])
+    servings = str
+    totalTime = str
+    ingredients = [str]
+
     page = requests.get("https://" + newList[index])
     soup1 = BeautifulSoup(page.content, "html.parser")
 
+    # Title
     title = soup1.find("span", {"class": 'o-AssetTitle__a-HeadlineText'}).text
 
+    # Total time
+    totalTitle = soup1.find("ul", {"class": 'o-RecipeInfo__m-Time'})
+    for li in totalTitle.find("span", {"class": 'o-RecipeInfo__a-Description m-RecipeInfo__a-Description--Total'}):
+        total_time = li.text
+
+    # Servings
+    yieldTitle = soup1.find("ul", {"class": 'o-RecipeInfo__m-Yield'})
+    for li in yieldTitle.find("span", {"class": 'o-RecipeInfo__a-Description'}):
+        if li.text.__contains__("servings"):
+            servings = li.text
+
+    # Ingredients
+    ingredientInfo = soup1.find_all("span", {"class": 'o-Ingredients__a-Ingredient--CheckboxLabel'})
+    for item in ingredientInfo:
+        if item.text == "Deselect All":
+            print(item.text)
+        else:
+            ingredients.append(item)
+
+    # Source and Website
     results = soup1.find("title").text.replace("\"", "").replace("\'", "\"").split("|")
-    source = results[1].rstrip().lstrip()
-    website = results[2].rstrip().lstrip()
+
+    try:
+        source = results[1].rstrip().lstrip()
+    except IndexError:
+        print("source index error")
+
+    try:
+        website = results[2].rstrip().lstrip()
+    except IndexError:
+        print("website index error")
+
+    # Setting all recipe information
     recipe.id = index
     recipe.title = title
     recipe.source = source
+    recipe.servings = servings
     recipe.website = website
+    recipe.total_time = total_time
+    recipe.ingredients = ingredients
 
     fnList.append(recipe)
-
-print(fnList)
---
+++ recipe.py
@@ -0,0 +1,9 @@
+
+class Recipe:
+    id: int
+    title: str
+    source: str
+    servings: str
+    website: str
+    total_time: str
+    ingredients: [str]
